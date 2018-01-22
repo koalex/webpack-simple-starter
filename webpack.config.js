3a5e9acbdf505bd1a0aa4c22c856e72551d32f2d
Konstantin Aleksandrov <aleksandrovkm@gmail.com>
2018-01-22 21:19:52 +0700

    Fix config

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: __DEV__ ? './' : '/',
+        publicPath: __DEV__ ? '/' : './',
         filename: '[name].js',
         chunkFilename: '[id].js'
     },
